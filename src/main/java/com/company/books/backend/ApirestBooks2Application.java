d22f2d71061a13ecf1b2e98175d53ba37c52592a
Jorge Meza Bello <jorberiver@gmail.com>
2022-09-06 00:59:15 -0500

    Inicio del proyecto

---
+++ src/main/java/com/company/books/backend/ApirestBooks2Application.java
@@ -0,0 +1,13 @@
+package com.company.books.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApirestBooks2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApirestBooks2Application.class, args);
+	}
+
+}
